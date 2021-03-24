469c67e5bc29434c8934289b246837c3e456b906
stephensalim <ssalim@hutanrimba.net>
2021-03-24 15:10:48 +1100

    sssalim_ops_automation_playbook_runbook

--- static/Operations/200_Automating_operations_with_playbooks_and_runbooks/Code/scripts/build_application.sh
+++ static/Operations/200_Automating_operations_with_playbooks_and_runbooks/Code/scripts/build_application.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
-BASE_STACK=$1
-LABEL=$2
+LABEL='latest'
+ECR_REPONAME='walab-ops-sample-application'
+SAMPLE_APPNAME=$ECR_REPONAME
+MAIN_STACK=$1
+EMAIL=$2
 
-ECR_REPONAME=$(aws cloudformation describe-stacks --stack-name $BASE_STACK --query 'Stacks[0].Outputs[?OutputKey==`OutputPattern1AppContainerRepository`].OutputValue |[0]' | sed 's/"//g')
-STACK_ID=$(aws cloudformation describe-stacks --stack-name $BASE_STACK --query 'Stacks[0].StackId' | sed 's/"//g')
+sudo yum install jq -y -q
+AWS_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq '.region' | sed -e 's/^"//' -e 's/"$//')
+AWS_ACCOUNT=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq '.accountId' | sed -e 's/^"//' -e 's/"$//')
+RESOURCEID=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq '.instanceId' | sed -e 's/^"//' -e 's/"$//')
 
-echo $ECR_REPONAME
+aws ec2 describe-tags --filters Name=resource-id,Values=$RESOURCEID Name=tagkey,Values=aws:cloudformation:stack-name
 
-IFS=':'
-read -ra ADDR <<< "$STACK_ID" # str is read into an array as tokens separated by IFS
+echo '#################################################'
+echo 'Script will deploy application with below details'
+echo '#################################################'
+echo 'Region: ' $AWS_REGION
+echo 'Account: '$AWS_ACCOUNT
+echo 'Repo Name: '$ECR_REPONAME
+echo 'Label: '$LABEL
 
-AWS_REGION=${ADDR[3]}
-AWS_ACCOUNT=${ADDR[4]}
-echo $AWS_REGION
-echo $AWS_ACCOUNT
-
+echo '##############################'
+echo 'Building Application Container'
+echo '##############################'
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com
-docker build -t $ECR_REPONAME .
+docker build -t $ECR_REPONAME ../src/
 docker tag $ECR_REPONAME:latest $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPONAME:$LABEL
 docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPONAME:$LABEL
 
+echo '########################'
+echo 'Deploy Application Stack'
+echo '########################'
+aws iam create-service-linked-role --aws-service-name ecs.amazonaws.com
+aws cloudformation create-stack --stack-name $ECR_REPONAME \
+                                --template-body file://../templates/base_app.yml \
+                                --parameters ParameterKey=BaselineVpcStack,ParameterValue=$MAIN_STACK \
+                                            ParameterKey=ECRImageURI,ParameterValue=$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPONAME:$LABEL \
+                                            ParameterKey=NotificationEmail,ParameterValue=$EMAIL \
+                                --capabilities CAPABILITY_NAMED_IAM \
+                                --tags Key=Application,Value=$SAMPLE_APPNAME
 
-echo "==========================="
-echo "Image URI:" $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPONAME:$LABEL
-
+echo '#########################################'
+echo 'Waiting for Application Stack to complete'
+echo '#########################################'
+aws cloudformation wait stack-create-complete --stack-name $ECR_REPONAME
